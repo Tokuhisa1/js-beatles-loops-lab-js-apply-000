534c208031ed37b28adaa56861e505434fa95f88
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-06-16 18:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const theBeatlesPlay = (musicians, instruments) => {
-  let intro = [];
-
-  for (let i = 0; i < musicians.length; i += 1) {
-    intro.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-
-  return intro;
-};
+// const theBeatlesPlay = (musicians, instruments) => {
+//   let intro = [];
+//
+//   for (let i = 0; i < musicians.length; i += 1) {
+//     intro.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+//
+//   return intro;
+// };
